188cf4d8dae802021d091e3bd88eacfce4259af4
arunb0022 <60004935+arunb0022@users.noreply.github.com>
2020-01-24 19:14:22 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Example1 from "./Example1";
+import Example2 from "./Example2";
+import Example3 from "./Example3";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Example1 />
+        <Example2 />
+        <Example3 />
+      </div>
+    );
+  }
 }
 
 export default App;
